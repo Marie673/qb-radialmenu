3afaaee0da3a94ffadc1d8d44328b1275938a433
Okazaki Kohei <marie673g@gmail.com>
2023-08-26 16:19:57 +0900

    Update jp.lua

--- locales/jp.lua
+++ locales/jp.lua
@@ -1,57 +1,57 @@
 local Translations = {
     error = {
-        no_people_nearby = "No players nearby",
-        no_vehicle_found = "No vehicle found",
-        extra_deactivated = "Extra %{extra} has been deactivated",
-        extra_not_present = "Extra %{extra} is not present on this vehicle",
-        not_driver = "You're not the driver of the vehicle",
-        vehicle_driving_fast = "This vehicle is going too fast",
-        seat_occupied = "This seat is occupied",
-        race_harness_on = "You have a race harness on, you can't switch",
-        obj_not_found = "Couldn't create the requested object",
-        not_near_ambulance = "You're not near an Ambulance",
-        far_away = "You're too far away",
-        stretcher_in_use = "This stretcher is already in use",
-        not_kidnapped = "You did not kidnap this person",
-        trunk_closed = "The trunk is closed",
-        cant_enter_trunk = "You can't get in this trunk",
-        already_in_trunk = "You're already in the trunk",
-        someone_in_trunk = "Someone is already in the trunk"
+        no_people_nearby = "近くにプレイヤーがいません",
+        no_vehicle_found = "乗り物が見つかりません",
+        extra_deactivated = "%{extra} が無効になりました",
+        extra_not_present = "%{extra} はこの車両には存在しません",
+        not_driver = "この車の運転手ではありません",
+        vehicle_driving_fast = "この車両は早すぎます",
+        seat_occupied = "席が埋まっています",
+        race_harness_on = "レース用ハーネスを装着しているため、切り替えることはできません",
+        obj_not_found = "要求されたオブジェクトを作成できませんでした",
+        not_near_ambulance = "救急車の近くにいない",
+        far_away = "遠すぎます",
+        stretcher_in_use = "この担架はすでに使用されています",
+        not_kidnapped = "この人物を誘拐できません",
+        trunk_closed = "トランクは閉まっています",
+        cant_enter_trunk = "このトランクに入ることができません",
+        already_in_trunk = "すでにトランクの中にいます",
+        someone_in_trunk = "誰かがトランクに入っています"
     },
     progress = {
-        flipping_car = "Flipping vehicle.."
+        flipping_car = "車両をひっくり返しています"
     },
     success = {
-        extra_activated = "Extra %{extra} has been activated",
-        entered_trunk = "You're in the trunk"
+        extra_activated = "%{extra} が有効になりました",
+        entered_trunk = "トランクの中にいます"
     },
     info = {
-        no_variants = "There don't seem to be any variants for this",
-        wrong_ped = "This ped model does not allow for this option",
-        nothing_to_remove = "You don't appear to have anything to remove",
-        already_wearing = "You are already wearing that",
-        switched_seats = "You are now on the %{seat}"
+        no_variants = "バリエーションがありません",
+        wrong_ped = "この ped モデルではこのオプションは使用できません",
+        nothing_to_remove = "削除するものがありません",
+        already_wearing = "すでに着ています",
+        switched_seats = "%{seat} に座っています"
     },
     general = {
-        command_description = "Open Radial Menu",
-        push_stretcher_button = "[E] - Push Stretcher",
-        stop_pushing_stretcher_button = "~g~E~w~ - Stop Pushing",
-        lay_stretcher_button = "[G] - Lay On Stretcher",
+        command_description = "ラジアルメニューを開く",
+        push_stretcher_button = "[E] - ストレッチャーを押す",
+        stop_pushing_stretcher_button = "~g~E~w~ - 押すのをやめる",
+        lay_stretcher_button = "[G] - ストレッチャーに横たわる",
         push_position_drawtext = "Push Here",
-        get_off_stretcher_button = "[G] - Get Off Stretcher",
-        get_out_trunk_button = "[E] Get out of the trunk",
-        close_trunk_button = "[G] Close the trunk",
-        open_trunk_button = "[G] Open the trunk",
-        getintrunk_command_desc = "Get In Trunk",
-        putintrunk_command_desc = "Put Player In Trunk"
+        get_off_stretcher_button = "[G] - ストレッチャーから降りる",
+        get_out_trunk_button = "[E] トランクから出る",
+        close_trunk_button = "[G] トランクを閉める",
+        open_trunk_button = "[G] トランクを開ける",
+        getintrunk_command_desc = "トランクに入る",
+        putintrunk_command_desc = "プレイヤーをトランクに入れる"
     },
     options = {
-        emergency_button = "Emergency Button",
-        driver_seat = "Drivers Seat",
+        emergency_button = "緊急",
+        driver_seat = "運転席",
         passenger_seat = "Passenger Seat",
-        other_seats = "Other Seat",
-        rear_left_seat = "Rear Left Seat",
-        rear_right_seat = "Rear Right Seat"
+        other_seats = "助手席",
+        rear_left_seat = "後部左席",
+        rear_right_seat = "後部右席"
     },
 }
 
